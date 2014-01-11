484a19d2f1032f4f5acd6042aa5c76647e34191d
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2014-01-11 01:23:32 +0100

    All implemented and tested

--- list.sh
+++ list.sh
@@ -1,4 +1,37 @@
-# Constructs a list from a string where elements are space separated
+__number_of_underscores() {
+  echo $@ | awk -F_ '{ print NF-1 }'
+}
+
+__desugar_function_1() {
+  if [ $(__number_of_underscores $@) -eq 1 ]; then
+    echo $@ | sed 's/_1\?/${x}/g'
+  else
+    echo $@ | sed 's/_1/${x}/g'
+  fi
+}
+
+__desugar_function_2() {
+  if [ $(__number_of_underscores $@) -eq 2 ]; then
+    echo $@ | sed '
+      s/_1/${x}/g
+      s/_2/${y}/g
+      s/_/${x}/
+      s/_/${y}/'
+  else
+    echo $@ | sed '
+      s/_1/${x}/g
+      s/_2/${y}/g'
+  fi
+}
+
+__swap_args() {
+  __desugar_function_2 $@ | sed -e '
+    s/${x}/${z}/g
+    s/${y}/${x}/g
+    s/${z}/${y}/g'
+}
+
+# Constructs a list from arguments
 # def list: List[String]
 list() {
   for i in "$@"; do echo "$i"; done
@@ -7,7 +40,7 @@ list() {
 # Selects an element by its index in the list.
 # def apply(n: Int): A
 apply() {
-  for _ in $(seq 0 $1); do read x; done
+  for x in $(seq 0 $1); do read x; done
   echo "$x"
 }
 
@@ -23,86 +56,115 @@ contains() {
 # Counts the number of elements in the list which satisfy a predicate.
 # def count(p: String => Boolean): Int
 count() {
-  :
+  i=0
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval test $f && i=$(expr $i + 1)
+  done
+  echo $i
 }
 
 # Selects all elements except first `n` ones.
 # def drop(n: Int): List[String]
 drop() {
-  for _ in $(seq 1 $1); do read x; done
-  while read x; do echo "$x"; done
+  tail -n$1
 }
 
 # Drops longest prefix of elements that satisfy a predicate.
 # def dropwhile(pred: String => Boolean): List[String]
 dropwhile() {
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval test $f || break
+  done
+  echo $x
+  cat
 }
 
 # Tests whether a predicate holds for some of the elements of this list.
 # def exists(pred: String => Boolean): Boolean
 exists() {
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval test $f && return 0
+  done
+  return 1
 }
 
 # Selects all elements of this list which satisfy a predicate.
 # def filter(pred: String => Boolean): List[String]
 filter() {
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval test $f && echo $x
+  done
 }
 
 # Selects all elements of this list which do not satisfy a predicate.
 # def filternot(pred: String => Boolean): List[String]
 filternot() {
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval test $f || echo $x
+  done
 }
 
 # Finds the first element of the list satisfying a predicate.
 # def find(pred: String => Boolean): String
 find() {
-  # Conflict!
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval test $f && echo $x && break
+  done
 }
 
 # Folds the elements of this list using the specified associative binary
 # operator.
-# def fold(z: String)(op: (String, String) => String): String
+# def fold(z: String, op: (String, String) => String): String
 fold() {
-  # Conflict!
-  :
+  foldleft $@
 }
 
 # Applies a binary operator to a start value and all elements of this list,
 # going left to right.
-# def foldleft(z: String)(op: (String, String) => String): String
+# def foldleft(z: String, op: (String, String) => String): String
 foldleft() {
-  :
+  x=$1
+  shift 1
+  f=$(__desugar_function_2 $@)
+  while read y; do
+    x=$(eval $f)
+  done
+  echo $x
 }
 
 # Applies a binary operator to all elements of this list and a start value,
 # going right to left.
-# def foldright(z: String)(op: (String, String) => String): String
+# def foldright(z: String, op: (String, String) => String): String
 foldright() {
-  :
+  reverse | foldleft $(__swap_args $@)
 }
 
 # Tests whether a predicate holds for all elements of this list.
 # def forall(pred: String => Boolean): Boolean
 forall() {
-  :
-}
-
-# Applies a function `f` to all elements of this list.
-# def foreach[U](f: String => U): Unit
-foreach() {
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    ! eval test $f && return 1
+  done
+  return 0
 }
 
 # Selects the first element of this list.
+# Uses /usr/bin/head when called with arguments.
 # def head: String
 head() {
-  read x
-  echo "$x"
+  if [ $# -eq 0 ]; then
+    read x
+    echo "$x"
+  else
+    /usr/bin/head $@
+  fi
 }
 
 # Selects all elements except the last.
@@ -121,30 +183,30 @@ isempty() {
 # def last: String
 last() {
   # Conflict!
-  sed -n '$p'
+  tail -n1
 }
 
 # The length of the list.
 # def length: Int
 length() {
-  wc -l
+  wc -l # || awk 'END{print NR}'
 }
 
 # Builds a new collection by applying a function to all elements of this
 # list.
 # def map(f: String => String): String
 map() {
-  :
+  f=$(__desugar_function_1 $@)
+  while read x; do
+    eval $f
+  done
 }
 
 # Displays all elements of this list in a string using a separator string.
 # def mkstring(sep: String = " "): String
 mkstring() {
-  if [ $# -eq 0 ]; then
-    tr '\n' ' ' | sed 's/.$//'
-  else
-    tr '\n' $1 | sed 's/.$//'
-  fi
+  char=$1
+  tr '\n' "${char:= }" | sed 's/.$//'
 }
 
 # Tests whether the list is not empty.
@@ -157,70 +219,77 @@ nonempty() {
 # operator.
 # def reduce(op: (String, String) => String): String
 reduce() {
-  :
+  reduceleft $@
 }
 
 # Applies a binary operator to all elements of this list going left to
 # right.
 # def reduceleft(op: (String, String) => String): String
 reduceleft() {
-  :
+  read x
+  foldleft $x $@
 }
 
 # Applies a binary operator to all elements of this list, going right to
 # left.
 # def reduceright(op: (String, String) => String): String
 reduceright() {
-  :
+  reverse | reduceleft $(__swap_args $@)
 }
 
 # Returns new list with elements in reversed order.
 # def reverse: List[String]
 reverse() {
-  tac
+  tac 2> /dev/null || sed '1!G;h;$!d'
 }
 
 # Selects all elements except the first.
+# Uses /usr/bin/tail when called with arguments.
 # def tail: List[String]
 tail() {
-  # Conflict!
-  :
+  if [ $# -eq 0 ]; then
+    read x
+    cat
+  else
+    /usr/bin/tail $@
+  fi
 }
 
 # Selects first `n` elements.
 # def take(n: Int): List[String]
 take() {
-  :
+  head -n$1
 }
 
 # Selects last n elements.
 # def takeright(n: Int): List[String]
 takeright() {
-  :
+  tail -n$1
 }
 
 # Takes longest prefix of elements that satisfy a predicate.
 # def takewhile(pred: String => Boolean): List[String]
 takewhile() {
-  :
+  while read x; do
+    eval test $(__desugar_function_1 $@) || break
+    echo $x
+  done
 }
 
 # A copy of this list with one single replaced element.
 # def updated(index: Int, elem: String): List[String]
 updated() {
-  :
-}
-
-# Returns a list formed from this list and another iterable collection by
-# combining corresponding elements in pairs.
-# def zip(that: List[String]): List[(String, String)]
-zip() {
-  # Conflict!
-  :
+  for x in $(seq 1 $1); do
+    read x
+    echo "$x"
+  done
+  echo "$2"
+  read x
+  cat
 }
 
 # Zips this list with its indices.
 # def zipwithindex: List[(String, Int)]
 zipwithindex() {
-  :
+  awk '{ print $0 " " FNR }'
 }
--- test.sh
+++ test.sh
@@ -1,23 +1,27 @@
 NONE='\033[00m'
+BOLD='\033[1m'
 RED='\033[01;31m'
 GREEN='\033[01;32m'
-BOLD='\033[1m'
 passed=0
 failed=0
 
 check() {
+  shells='ash bash dash ksh mksh pdksh posh zsh'
+  f='tail head tac wc'
   runing=$1
   expected=$2
-  got=$(sh -c ". ./list.sh; $runing")
-  if [ "$got" = "$expected" ]; then
-    passed=$(expr 1 + $passed)
-  else
-    echo "\
-${BOLD}Runing:${NONE}\n$runing
-${BOLD}Expected:${NONE}\n$expected
-${BOLD}Got:${NONE}\n$got"
-    failed=$(expr 1 + $failed)
-  fi
+  for shell in $shells; do
+    got=$($shell -c ". ./list.sh; $runing")
+    if [ "$got" = "$expected" ]; then
+      passed=$(expr 1 + $passed)
+    else
+      echo "\
+${BOLD}-Running on $shell:${NONE}\n$runing
+${BOLD}-Expected:${NONE}\n$expected
+${BOLD}-Got:${NONE}\n$got"
+      failed=$(expr 1 + $failed)
+    fi
+  done
 }
 
 checktrue() {
@@ -45,41 +49,51 @@ printresult() {
   echo $NONE
 }
 
-check 'list "j e" "t u" "vo us"' 'j e
-t u
-vo us'
-
 check 'list "j e" "t u" "vo us" | apply 1' 't u'
-
 checktrue 'list 1 2 | contains 1'
-
 checkfalse 'list 1 2 | contains 3'
-
-check 'list 1 2 3 4 | drop 2' '3
-4'
-
+check 'list 1 2 3 4 | drop 2 | mkstring' '3 4'
 check 'list 1 2 | head' '1'
-
-check 'list "j e" "t u" "vo us" | init' 'j e
-t u'
-
+check 'list "j e" "t u" "vo us" | init | mkstring' 'j e t u'
 checkfalse 'list 1 2 3 | isempty'
-
 checktrue 'list 1 2 3 | nonempty'
-
 checktrue 'echo -n | isempty'
-
 checkfalse 'echo -n | nonempty'
-
 check 'list 1 2 | last' '2'
-
 check 'list 1 2 3 | length' '3'
-
 check 'list 1 2 3 | mkstring' '1 2 3'
-
 check 'list 1 2 3 | mkstring ,' '1,2,3'
-
 check 'list 1 2 3 | reverse | mkstring' '3 2 1'
-
+check 'list 1 2 3 | take 2 | mkstring' '1 2'
+check 'list 1 2 3 | tail | mkstring' '2 3'
+check 'list 1 2 3 | takeright 2 | mkstring' '2 3'
+check 'list 1 2 3 | map echo s_ | mkstring' 's1 s2 s3'
+check 'list 1 2 3 | map echo s_1 | mkstring' 's1 s2 s3'
+check 'list 1 2 3 | updated 1 g | mkstring' '1 g 3'
+check 'list a b c | zipwithindex | mkstring' 'a 1 b 2 c 3'
+check 'list 1 2 3 4 | filter _ -ge 3 | mkstring' '3 4'
+check 'list 1 2 3 4 | filternot _ -ge 3 | mkstring' '1 2'
+checktrue 'list 1 2 3 4 | exists _ = 3'
+checkfalse 'list 1 2 3 4 | exists _ = 5'
+check 'list 1 2 3 4 | count _ -ge 2' '3'
+check 'list 1 2 3 4 | dropwhile _ -le 2 | mkstring' '3 4'
+checktrue 'list 1 2 3 4 | forall _ -ge 1'
+checkfalse 'list 1 2 3 4 | forall _ -ge 2'
+check 'list 1 2 3 4 | takewhile _ -le 2 | mkstring' '1 2'
+check 'list 1 2 3 4 | find _ -ge 2' '2'
+check 'list 4 2 4 | foldleft 64 expr _1 / _2' '2'
+check 'list 4 2 4 | foldleft 64 expr _ / _' '2'
+check 'list 34 56 12 4 23 | foldleft 7 expr _1 / _2' '0'
+check 'list 34 56 12 4 23 | foldleft 7 expr _ / _' '0'
+check 'list 34 56 12 4 23 | fold 7 expr _ / _' '0'
+check 'list 34 56 12 4 23 | foldright 7 expr _1 / _2' '8'
+check 'list 34 56 12 4 23 | foldright 7 expr _ / _' '8'
+check 'list 64 4 2 4 | reduceleft expr _ / _' '2'
+check 'list 64 4 2 4 | reduceleft expr _ / _' '2'
+check 'list 7 34 56 12 4 23 | reduceleft expr _1 / _2' '0'
+check 'list 7 34 56 12 4 23 | reduceleft expr _ / _' '0'
+check 'list 7 34 56 12 4 23 | reduce expr _ / _' '0'
+check 'list 34 56 12 4 23 7 | reduceright expr _1 / _2' '8'
+check 'list 34 56 12 4 23 7 | reduceright expr _ / _' '8'
 
 printresult
